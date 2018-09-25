7e78fb446fa8a2aed413674c4422f6f206a09eae
Benjamin Lepine <benjamin.lepine@epitech.eu>
2018-09-25 23:26:13 +0200

    commit

---
+++ src/todolist/Todolist.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Todolist extends Component {
+    render() {
+        return (
+            <h1>To Do List</h1>
+        );
+    }
+}
+
+export default Todolist;
