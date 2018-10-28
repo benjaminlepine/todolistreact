aff4a2bf8c75c6f67bba74406b6991c2cd6cf4de
Benjamin Lepine <benjamin.lepine@epitech.eu>
2018-10-28 18:23:28 +0100

    commit

--- src/todolist/Todolist.js
+++ src/todolist/Todolist.js
@@ -1,9 +1,70 @@
 import React, { Component } from 'react';
 
 class Todolist extends Component {
+    constructor(){
+        super();
+        this.state = {
+            userInput: '',
+            items: []
+        }
+    }
+
+    onChange(event){
+        this.setState({
+            userInput: event.target.value
+        })
+    }
+
+    addToDo(event){
+        event.preventDefault()
+        this.setState({
+            userInput:'',
+            items :[...this.state.items, this.state.userInput]
+        }, ()=>console.log(this.state))
+    }
+
+    deleteItem(item){
+        let array = this.state.items;
+        let index = array.indexOf(item)
+        array.splice(index,1)
+        this.setState({
+            items:array
+        });
+    }
+
+    renderToDo(){
+        return this.state.items.map((item)=>{
+            return(
+                <div key={item}>
+                    {item}  <button className="btn btn-secondary"
+                                    onClick={this.deleteItem.bind(this, item)}>X
+                            </button>
+                </div>
+            )
+        })
+    }
+
     render() {
         return (
-            <h1>To Do List</h1>
+            <div>
+                <h1>To Do List</h1>
+                <form>
+                    <input
+                        className="form-control mb-2"
+                        value={this.state.userInput}
+                        type="text"
+                        placeholder="Renseigner un item"
+                        onChange={this.onChange.bind(this)}
+                    />
+                    <button
+                        className="btn btn-primary"
+                        onClick={this.addToDo.bind(this)}>Ajouter
+                    </button>
+                </form>
+                <div>
+                    {this.renderToDo()}
+                </div>
+            </div>
         );
     }
 }
